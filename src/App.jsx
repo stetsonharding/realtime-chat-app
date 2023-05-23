c27203667fff2621707b8bc542ae538e968c480f
stetsonharding <stetson2001@msn.com>
2023-05-23 07:59:28 -0700

    Getting users authentication info.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-// import './App.css'
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
 import Auth from "./components/Auth";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+
   return (
     <div>
       <Auth />
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,10 +1,20 @@
 // import React from 'react'
 import { auth, provider } from "../firebase-config.jsx";
 import { signInWithPopup } from "firebase/auth";
+import Cookies from "universal-cookie";
 
 function Auth() {
+  //Authentication
   const signInWithGoogle = async () => {
-    await signInWithPopup(auth, provider);
+    try {
+      //Getting User info upon login
+      const result = await signInWithPopup(auth, provider);
+      //setting cookie so user is not logged out upon refresh
+      const cookies = new Cookies();
+      cookies.set("auth-token", result.user.refreshToken);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
