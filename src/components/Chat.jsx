12c41a48fd90bf8d1e8db761d0fcab113f6fac1f
stetsonharding <stetson2001@msn.com>
2023-05-30 07:19:27 -0700

    snapshot to fetch messages in db

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import { auth, db } from "../firebase-config";
 import "../css/Chat.css";
 
 import PropTypes from "prop-types";
 
 function Chat({ room }) {
+  //storing users typed message
   const [message, setMessage] = useState("");
 
+  //Getting the database collection => database and collection of messages
   const messagesRef = collection(db, "messages");
 
+  useEffect(() => {
+    //query for database
+    const queryMessages = query(messagesRef, where("room", "==", room));
+
+    //Get messages in real time
+    onSnapshot(queryMessages, (snapshot) => {
+      console.log("new message");
+    });
+  }, []);
+
+  //Adding users message/object to firebase/firestore
   const handleSubmitForm = async (e) => {
     e.preventDefault();
     if (message) {
